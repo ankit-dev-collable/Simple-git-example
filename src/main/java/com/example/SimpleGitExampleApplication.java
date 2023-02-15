929d71a9f1411e1ab0f6a68173a58a78be51f829
ankit-dev-collable <gnit108@gmail.com>
2023-02-15 15:51:07 +0530

    15:51

--- src/main/java/com/example/SimpleGitExampleApplication.java
+++ src/main/java/com/example/SimpleGitExampleApplication.java
@@ -9,13 +9,9 @@ public class SimpleGitExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleGitExampleApplication.class, args);
 
-		System.out.println("SimpleGitExampleApplication");
-
-		System.out.println("code changes SimpleGitExampleApplication");
-
-		System.out.println("code changes SimpleGitExampleApplication SIMPLE-GIT-EXAMPLE");
-		System.out.println("code changes IN LINE NUMBER 15");
+        System.out.println("code changes time 12-02-2023 at 15:49");
 
 	}
 
 }
+

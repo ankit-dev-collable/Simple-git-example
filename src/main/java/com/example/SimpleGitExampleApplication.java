9b1a937e6431ca115150571b80741d62aa64b447
ankit-dev-collable <gnit108@gmail.com>
2023-02-15 15:54:57 +0530

    code changes at 51:53

--- src/main/java/com/example/SimpleGitExampleApplication.java
+++ src/main/java/com/example/SimpleGitExampleApplication.java
@@ -9,7 +9,7 @@ public class SimpleGitExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleGitExampleApplication.class, args);
 		
-		System.out.println("SimpleGitExampleApplication");
+		System.out.println("code changes at 51:53 in feature branch");
 
 	}
 

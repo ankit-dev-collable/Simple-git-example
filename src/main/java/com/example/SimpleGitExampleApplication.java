0cecc07c38bafc01e0a1937ce1d86fefdfa2e038
ankit-dev-collable <55022847+ankit-dev-collable@users.noreply.github.com>
2023-02-15 16:12:45 +0530

    Update SimpleGitExampleApplication.java

--- src/main/java/com/example/SimpleGitExampleApplication.java
+++ src/main/java/com/example/SimpleGitExampleApplication.java
@@ -10,10 +10,9 @@ public class SimpleGitExampleApplication {
 		SpringApplication.run(SimpleGitExampleApplication.class, args);
 
 
-		System.out.println("code changes time 12-02-2023 at 16:05");
+		System.out.println("code changes time 12-02-2023 at 16:13");
 
 
-		System.out.println("code changes at 16:03 in feature branch");
 
 
 	}

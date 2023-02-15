0214848c7f00c06dc936a868c2fae483831e7753
ankit-dev-collable <gnit108@gmail.com>
2023-02-15 14:27:49 +0530

    code changes in main application class

--- src/main/java/com/example/SimpleGitExampleApplication.java
+++ src/main/java/com/example/SimpleGitExampleApplication.java
@@ -8,6 +8,7 @@ public class SimpleGitExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleGitExampleApplication.class, args);
+		System.out.println("changes in line number 11");
 	}
 
 }
